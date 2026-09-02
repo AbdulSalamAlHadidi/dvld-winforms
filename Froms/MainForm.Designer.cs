7363ba05f2630488b49a8ef7092e6239bdc8bb59
Abdulslam AlHadidi <slmhadidi@gmail.com>
2026-09-02 18:36:25 +0300

    feat: add main application shell layout

--- Froms/MainForm.Designer.cs
+++ Froms/MainForm.Designer.cs
@@ -28,7 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlSidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblDVLD = new System.Windows.Forms.Label();
+            this.pnlTopbar = new System.Windows.Forms.Panel();
+            this.lblPageTitle = new System.Windows.Forms.Label();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.lblWelcometoDVLD = new System.Windows.Forms.Label();
+            this.pnlSidebar.SuspendLayout();
+            this.pnlTopbar.SuspendLayout();
+            this.pnlContent.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlSidebar
+            // 
+            this.pnlSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.pnlSidebar.Controls.Add(this.lblDVLD);
+            this.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSidebar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSidebar.Name = "pnlSidebar";
+            this.pnlSidebar.Padding = new System.Windows.Forms.Padding(16, 20, 16, 20);
+            this.pnlSidebar.Size = new System.Drawing.Size(250, 681);
+            this.pnlSidebar.TabIndex = 0;
+            // 
+            // lblDVLD
+            // 
+            this.lblDVLD.AutoSize = true;
+            this.lblDVLD.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDVLD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(250)))), ((int)(((byte)(251)))));
+            this.lblDVLD.Location = new System.Drawing.Point(19, 20);
+            this.lblDVLD.Name = "lblDVLD";
+            this.lblDVLD.Size = new System.Drawing.Size(204, 90);
+            this.lblDVLD.TabIndex = 1;
+            this.lblDVLD.Text = "DVLD \nDriver & Vehicle License Department";
+            // 
+            // pnlTopbar
+            // 
+            this.pnlTopbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
+            this.pnlTopbar.Controls.Add(this.lblPageTitle);
+            this.pnlTopbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopbar.Location = new System.Drawing.Point(250, 0);
+            this.pnlTopbar.Name = "pnlTopbar";
+            this.pnlTopbar.Padding = new System.Windows.Forms.Padding(24, 0, 24, 0);
+            this.pnlTopbar.Size = new System.Drawing.Size(934, 70);
+            this.pnlTopbar.TabIndex = 1;
+            // 
+            // lblPageTitle
+            // 
+            this.lblPageTitle.AutoSize = true;
+            this.lblPageTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPageTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(250)))), ((int)(((byte)(251)))));
+            this.lblPageTitle.Location = new System.Drawing.Point(24, 22);
+            this.lblPageTitle.Name = "lblPageTitle";
+            this.lblPageTitle.Size = new System.Drawing.Size(118, 30);
+            this.lblPageTitle.TabIndex = 0;
+            this.lblPageTitle.Text = "Dashboard";
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Controls.Add(this.lblWelcometoDVLD);
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(250, 70);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Padding = new System.Windows.Forms.Padding(24);
+            this.pnlContent.Size = new System.Drawing.Size(934, 611);
+            this.pnlContent.TabIndex = 2;
+            // 
+            // lblWelcometoDVLD
+            // 
+            this.lblWelcometoDVLD.AutoSize = true;
+            this.lblWelcometoDVLD.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcometoDVLD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(250)))), ((int)(((byte)(251)))));
+            this.lblWelcometoDVLD.Location = new System.Drawing.Point(357, 287);
+            this.lblWelcometoDVLD.Name = "lblWelcometoDVLD";
+            this.lblWelcometoDVLD.Size = new System.Drawing.Size(187, 30);
+            this.lblWelcometoDVLD.TabIndex = 1;
+            this.lblWelcometoDVLD.Text = "Welcome to DVLD";
             // 
             // MainForm
             // 
@@ -36,6 +110,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
             this.ClientSize = new System.Drawing.Size(1184, 681);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.pnlTopbar);
+            this.Controls.Add(this.pnlSidebar);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimumSize = new System.Drawing.Size(1200, 720);
@@ -43,10 +120,23 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DVLD";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlSidebar.ResumeLayout(false);
+            this.pnlSidebar.PerformLayout();
+            this.pnlTopbar.ResumeLayout(false);
+            this.pnlTopbar.PerformLayout();
+            this.pnlContent.ResumeLayout(false);
+            this.pnlContent.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel pnlSidebar;
+        private System.Windows.Forms.Panel pnlTopbar;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.Label lblPageTitle;
+        private System.Windows.Forms.Label lblWelcometoDVLD;
+        private System.Windows.Forms.Label lblDVLD;
     }
 }
