81e31add3325f6eae7ca511b3e886f716070b42d
Abdulslam AlHadidi <slmhadidi@gmail.com>
2026-09-03 12:50:17 +0300

    feat: add navigation service and dashboard placeholder page

---
+++ Navigation/NavigationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Desktop.Navigation
+{
+    internal class NavigationService
+    {
+        private readonly Panel _contentPanel;
+
+        public NavigationService(Panel contentPanel)
+        {
+            _contentPanel = contentPanel;
+            _contentPanel.Dock = DockStyle.Fill;
+        }
+
+        public void NavigateTo(UserControl page)
+        {
+            _contentPanel.Controls.Clear();
+            page.Dock = DockStyle.Fill;
+            _contentPanel.Controls.Add(page);
+        }
+
+
+    }
+}
