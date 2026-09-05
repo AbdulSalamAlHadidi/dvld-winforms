746f370d30516a7e88f7c6f72f5d7aa6e182419d
Abdulslam AlHadidi <slmhadidi@gmail.com>
2026-09-05 16:32:37 +0300

    feat: add reusable NavButton control with hover and active states

---
+++ Controls/NavButton.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DVLD.Desktop.Controls
+{
+    partial class NavButton
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.pnlAccent = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblText.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(161)))), ((int)(((byte)(170)))));
+            this.lblText.Location = new System.Drawing.Point(0, 0);
+            this.lblText.Name = "lblText";
+            this.lblText.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.lblText.Size = new System.Drawing.Size(250, 48);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Nav Item ";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pnlAccent
+            // 
+            this.pnlAccent.BackColor = System.Drawing.Color.Red;
+            this.pnlAccent.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlAccent.Location = new System.Drawing.Point(0, 0);
+            this.pnlAccent.Name = "pnlAccent";
+            this.pnlAccent.Size = new System.Drawing.Size(4, 48);
+            this.pnlAccent.TabIndex = 1;
+            this.pnlAccent.Visible = false;
+            // 
+            // NavButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.Controls.Add(this.pnlAccent);
+            this.Controls.Add(this.lblText);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "NavButton";
+            this.Size = new System.Drawing.Size(250, 48);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Panel pnlAccent;
+    }
+}
---
+++ Controls/NavButton.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Desktop.Controls
+{
+    public partial class NavButton : UserControl
+    {
+        private bool _isActive;
+        private static readonly Color ColorNormalBack = Color.FromArgb(33,33,33);
+        private static readonly Color ColorHoverBack = Color.FromArgb(39, 39, 42);
+        private static readonly Color ColorActiveBack = Color.FromArgb(109, 40, 217);
+        private static readonly Color ColorNormalText = Color.FromArgb(161, 161, 170);
+        private static readonly Color ColorActiveText = Color.FromArgb(249, 250, 251);
+        public event EventHandler NavClicked;
+        public NavButton()
+        {
+            InitializeComponent();
+            WireEvents();
+        }
+
+        public string NavText 
+        {
+            get => lblText.Text;
+            set => lblText.Text = value; 
+        }
+
+        public bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                _isActive = value;
+                ApplyVisualState();
+            }
+        }
+
+        private void ApplyVisualState()
+        {
+            if(_isActive)
+            {
+                BackColor = ColorActiveBack;
+                lblText.ForeColor = ColorActiveText;
+                pnlAccent.Visible = true;
+            }
+            else
+            {
+                BackColor = ColorNormalBack;
+                lblText.ForeColor = ColorNormalText;
+                pnlAccent.Visible = false;
+            }
+        }
+
+        private void WireEvents()
+        {
+            this.MouseEnter += NavButton_MouseEnter;
+            this.MouseLeave += NavButton_MouseLeave;
+            this.Click += NavButton_Click;
+
+            lblText.MouseEnter += NavButton_MouseEnter;
+            lblText.MouseLeave += NavButton_MouseLeave;
+            lblText.Click += NavButton_Click;
+        }
+
+        private void NavButton_MouseEnter(object sender,EventArgs e)
+        {
+            if (!_isActive)
+                BackColor = ColorHoverBack;
+        }
+        private void NavButton_MouseLeave(object sender,EventArgs e)
+        {
+            if (!_isActive)
+                BackColor = ColorNormalBack;
+        }
+        private void NavButton_Click(object sender,EventArgs e)
+        {
+            NavClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
