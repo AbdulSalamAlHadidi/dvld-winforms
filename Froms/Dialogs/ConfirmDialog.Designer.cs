be11d3f3c6029874f5cbbb82fd943c3b4dab32bd
Abdulslam AlHadidi <slmhadidi@gmail.com>
2026-09-06 17:35:01 +0300

    feat: add reusable ConfirmDialog for destructive actions

---
+++ Froms/Dialogs/ConfirmDialog.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DVLD.Desktop.Froms.Dialogs
+{
+    partial class ConfirmDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(250)))), ((int)(((byte)(251)))));
+            this.lblMessage.Location = new System.Drawing.Point(20, 30);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(360, 60);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "label1";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.FlatAppearance.BorderSize = 0;
+            this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirm.Location = new System.Drawing.Point(180, 110);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(90, 30);
+            this.btnConfirm.TabIndex = 1;
+            this.btnConfirm.Text = "button1";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(280, 110);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 30);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "button1";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // ConfirmDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(384, 141);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.lblMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConfirmDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ConfirmDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Froms/Dialogs/ConfirmDialog.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace DVLD.Desktop.Froms.Dialogs
+{
+    public partial class ConfirmDialog : Form
+    {
+        public ConfirmDialog(string title, string message, string confirmText = "Delete", string cancelText = "Cancel")
+        {
+            InitializeComponent();
+
+            Text = title;
+            lblMessage.Text = message;
+            btnConfirm.Text = confirmText;
+            btnCancel.Text = cancelText;
+
+            btnConfirm.BackColor = Color.FromArgb(239, 68, 68);
+            btnConfirm.ForeColor = Color.FromArgb(249, 250, 251);
+            btnCancel.BackColor = Color.FromArgb(55, 65, 81);
+            btnCancel.ForeColor = Color.FromArgb(249, 250, 251);
+
+            btnConfirm.Click += (s, e) => { DialogResult = DialogResult.Yes; Close(); };
+            btnCancel.Click += (s, e) => { DialogResult = DialogResult.Cancel; Close(); };
+        }
+
+        public static bool Confirm(string title, string message, string confirmText = "Delete")
+        {
+            using (var dialog = new ConfirmDialog(title, message, confirmText))
+            {
+                return dialog.ShowDialog() == DialogResult.Yes;
+            }
+        }
+    }
+}
